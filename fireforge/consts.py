805767da2dde6cabdd8a7fcfe91ac5605e0cc971
@a.taoudi <taoudiabdelbasset.contact@gmail.com>
2026-04-21 12:03:47 +0100

    fix: move resources inside package and fix template endpoint variable

--- fireforge/consts.py
+++ fireforge/consts.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 URL_PATH_PARAM_PATTERN = r"\{([^}]+)\}"
 DOTENV_PLACEHOLDER_PATTERN = r'\${([^}]+?)(?::([^}]*))?}'
